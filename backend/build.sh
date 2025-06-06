1f91d5994e25b01f5841b87b7bb8339abe35128b
Elefer3-33 <eleferrario3@gmail.com>
2025-06-06 07:51:16 +0200

    Initial commit

---
+++ backend/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Install TA-Lib C library
+apt-get update && apt-get install -y build-essential wget curl
+
+curl -LO http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
+tar -xvzf ta-lib-0.4.0-src.tar.gz
+cd ta-lib
+./configure --prefix=/usr
+make
+make install
+cd ..
+rm -rf ta-lib ta-lib-0.4.0-src.tar.gz
+
+chmod +x build.sh
+
